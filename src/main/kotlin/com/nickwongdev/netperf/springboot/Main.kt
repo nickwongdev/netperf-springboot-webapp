fc30ba39b1956b224f664d7527a23b7eea191db0
Nicholas Wong <nickwongdev@gmail.com>
2018-12-27 21:54:21 -0800

    Fixing formatting

--- src/main/kotlin/com/nickwongdev/netperf/springboot/Main.kt
+++ src/main/kotlin/com/nickwongdev/netperf/springboot/Main.kt
@@ -1,20 +1,21 @@
 package com.nickwongdev.netperf.springboot
 
+import com.nickwongdev.netperf.service.WorkService
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import com.nickwongdev.netperf.service.WorkService
 
 @SpringBootApplication
 class Main {
 
-    @Bean
-    fun workService(): WorkService {
-        return WorkService()
-    }
+	@Bean
+	fun workService(): WorkService {
+		return WorkService()
+	}
 }
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Main::class.java, *args)
+	SpringApplication.run(Main::class.java, *args)
 }
+
 
